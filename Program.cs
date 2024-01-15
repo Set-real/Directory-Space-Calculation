77e8c9cbc231991bca1eb880987ef814139868af
Максим Груздев <maestrosuper@mail.ru>
2024-01-15 19:52:11 +0300

    Finish!!!

--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
-﻿public class Program
+﻿using Directory_Space_Calculation;
+using System.Text;
+
+public class Program
 {
+    private static string? SizeInfo;
     static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.Unicode;
 
+        var currentDir = Directory.GetCurrentDirectory();
+
+        var sizeCounterThread = new Thread(CountSize);
+        sizeCounterThread.Start(currentDir);
+
+        Console.WriteLine("Выполняется");
+
+        while(sizeCounterThread.IsAlive)
+        {
+            Console.WriteLine(".");
+            Thread.Sleep(100);
+        }
+
+        Console.WriteLine("Выполнено");
+
+        Console.WriteLine($"Папка {currentDir} занимает {SizeInfo} на диске");
+        Console.Read();
+    }   
+    public static void CountSize(object? obj)
+    {
+        if (obj != null)
+        {
+            SizeInfo = SizeCounter.GetDirSize((string)obj);
+        }
     }
 }
